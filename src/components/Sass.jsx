aa40f57cd5f8907e30dc127781a2566b2374a0c1
Mirzogulyam <mirzogulyam@192.168.1.7>
2024-02-04 16:59:24 +0500

    admin test

--- src/components/Sass.jsx
+++ src/components/Sass.jsx
@@ -44,7 +44,7 @@ export default function Sass() {
       setAuthTokens(data)
       setUser(jwtDecode(data.access))
       localStorage.setItem('authTokens', JSON.stringify(data))
-      history('/test/bon')
+      history('/admin/bon')
     } else {
       alert('Something went wrong!')
     }
@@ -92,52 +92,37 @@ export default function Sass() {
 
   return (
     <div className="background d-flex align-items-center">
-      <div className="container text-center">
-        <form onSubmit={submitFunc}>
-          <img
-            className="mb-4 login_logo"
-            src="img/transparent_logo.png"
-            alt=""
-          />
+    <div className="container text-center"> 
+      <form onSubmit={submitFunc}>
+          <img className="mb-4 login_logo" src="img/transparent_logo.png" alt=""/>
           <h1 className="h4 mb-3 fw-normal login_text">Вход в сервис</h1>
 
           <div className="form-floating mb-1">
-            <input
+              <input
               type="text"
               name="username"
               className="form-control"
               id="floatingInput"
               aria-describedby="loginHelp"
               placeholder="login"
-            />
-            <label for="floatingInput">Логин</label>
+              />
+              <label for="floatingInput">Логин</label>
           </div>
           <div className="form-floating">
-            <input
+              <input 
               className="form-control"
               id="floatingPassword"
               type="password"
               name="password"
-              placeholder="Password"
-            />
-            <label for="floatingPassword">Пароль</label>
+              placeholder="Password"/>
+              <label for="floatingPassword">Пароль</label>
           </div>
-          <br />
-          <button className="btn btn-info w-100 py-2" type="submit">
-            Войти
-          </button>
-          <p className="mt-5 mb-3 text-light footer">
-            © 2024 Powered by{' '}
-            <a className="footer_powered" href="https://t.me/mgulyamov">
-              Gulyamov
-            </a>{' '}
-            and{' '}
-            <a className="footer_powered" href="https://t.me/Bomuratov">
-              Bomuratov
-            </a>
+          <br/>
+          <button className="btn btn-info w-100 py-2" type="submit">Войти</button>
+          <p className="mt-5 mb-3 text-light footer">© 2024 Powered by <a className='footer_powered' href='https://t.me/mgulyamov'>Gulyamov</a> and <a className='footer_powered' href='https://t.me/Bomuratov'>Bomuratov</a>
           </p>
-        </form>
-      </div>
+      </form>
     </div>
+  </div>
   )
 }
