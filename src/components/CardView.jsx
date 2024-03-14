99ea2d180acdb42468377d1b527651a36d20ba02
Mirzogulyam <mirzogulyam@Mirzogulyams-MacBook-Air.local>
2024-03-14 09:32:24 +0500

    card ready

--- src/components/CardView.jsx
+++ src/components/CardView.jsx
@@ -1,31 +1,44 @@
 import React from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence, useDragControls } from 'framer-motion'
 import { useSwipeable } from 'react-swipeable'
 
 export default function CardView({ item, open, setIsOpen }) {
-  const handlers = useSwipeable({
-    onSwipedDown: () => setIsOpen(false),
-    preventDefaultTouchmoveEvent: true,
-  })
+  // const handlers = useSwipeable({
+  //   onSwipedDown: () => setIsOpen(false),
+  //   preventDefaultTouchmoveEvent: true,
+  // })
+
+  const controls = useDragControls()
+  console.log(controls)
 
   return (
     <AnimatePresence>
       {open && (
         <motion.div
-        {...handlers}
-        initial={{ y: '100%' }}
-        animate={{ y: 0 }}
-        exit={{ y: '100%' }}
-        transition={{ duration: 0.3 }}
-        style={{
-          position: 'fixed',
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background: 'white',
-          padding: '20px',
-          boxShadow: '0px -5px 10px rgba(0, 0, 0, 0.1)',
-        }}
+          // {...handlers}
+          drag="y"
+          dragControls={controls}
+          dragElastic={0.1} // Настройка упругости перетаскивания
+          dragMomentum={false} // Отключение инерции перетаскивания
+          dragConstraints={{ top: 0, bottom: 0 }} // Ограничение перетаскивания карточки внутри окна
+          onDragEnd={(event, info) => {
+            if (info.offset.y > 100) {
+              setIsOpen(false) // Закрываем карточку, если она была перетащена вниз на расстояние больше 100px
+            }
+          }}
+          initial={{ y: '100%' }}
+          animate={{ y: 0 }}
+          exit={{ y: '100%' }}
+          transition={{ duration: 0.3 }}
+          style={{
+            position: 'fixed',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background: 'white',
+            padding: '20px',
+            boxShadow: '0px -5px 10px rgba(0, 0, 0, 0.1)',
+          }}
           className="card_view_motion"
         >
           <div className="container">
