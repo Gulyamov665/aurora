5be284c44bfddeca858123e3031ede4649124d49
Mirzogulyam <mirzogulyam@Mirzogulyams-MacBook-Air.local>
2024-03-14 10:12:39 +0500

    background opacity

--- src/components/CardView.jsx
+++ src/components/CardView.jsx
@@ -18,43 +18,45 @@ export default function CardView({ item, open, setIsOpen }) {
   return (
     <AnimatePresence>
       {open && (
-        <motion.div
-          drag="y"
-          dragControls={controls}
-          dragElastic={0.7} // Настройка упругости перетаскивания
-          dragMomentum={false} // Отключение инерции перетаскивания
-          dragConstraints={{ top: 0, bottom: 0 }} // Ограничение перетаскивания карточки внутри окна
-          onDragEnd={(event, info) => {
-            if (info.offset.y > 100) {
-              setIsOpen(false) // Закрываем карточку, если она была перетащена вниз на расстояние больше 100px
-            }
-          }}
-          initial={{ y: '100%' }}
-          animate={{ y: 0 }}
-          exit={{ y: '100%' }}
-          transition={{ duration: 0.3 }}
-          style={{
-            position: 'fixed',
-            left: 0,
-            right: 0,
-            bottom: 0,
-            background: 'white',
-            padding: '20px',
-            boxShadow: '0px -5px 10px rgba(0, 0, 0, 0.1)',
-          }}
-          className="card_view_motion"
-        >
-          <div className="container">
-            <div className="card_view">
-              <h2 style={{ paddingTop: '10px' }}>{item.name}</h2>
-              <img className="card_view_img" src={item.photo} alt="" />
-              <br />
-              <b>Описание</b>
-              <p>{item.description}</p>
-              <p className="card_view_price">{item.price}</p>
+        <div className="card_modal">
+          <motion.div
+            drag="y"
+            dragControls={controls}
+            dragElastic={0.7} // Настройка упругости перетаскивания
+            dragMomentum={false} // Отключение инерции перетаскивания
+            dragConstraints={{ top: 0, bottom: 0 }} // Ограничение перетаскивания карточки внутри окна
+            onDragEnd={(event, info) => {
+              if (info.offset.y > 100) {
+                setIsOpen(false) // Закрываем карточку, если она была перетащена вниз на расстояние больше 100px
+              }
+            }}
+            initial={{ y: '100%' }}
+            animate={{ y: 0 }}
+            exit={{ y: '100%' }}
+            transition={{ duration: 0.3 }}
+            style={{
+              position: 'fixed',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              background: 'white',
+              padding: '20px',
+              boxShadow: '0px -5px 10px rgba(0, 0, 0, 0.1)',
+            }}
+            className="card_view_motion"
+          >
+            <div className="container">
+              <div className="card_view">
+                <h2 style={{ paddingTop: '10px' }}>{item.name}</h2>
+                <img className="card_view_img" src={item.photo} alt="" />
+                <br />
+                <b>Описание</b>
+                <p>{item.description}</p>
+                <p className="card_view_price">{item.price}</p>
+              </div>
             </div>
-          </div>
-        </motion.div>
+          </motion.div>
+        </div>
       )}
     </AnimatePresence>
   )
