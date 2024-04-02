2ea9c1f0529f432de37007ee2d59bd9b543cc5f6
Mirzogulyam <mirzogulyam@Mirzogulyams-MacBook-Air.local>
2024-04-03 00:51:12 +0500

    sticky header admin

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -32,7 +32,7 @@ export default function Header({ sidebar}) {
   }, [authTokens])
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} sticky-top`}>
       <div>
         <MenuIcon
           sx={{
