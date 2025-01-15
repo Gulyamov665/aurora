1bdf977283a8c31fd4fc38f3d1ba16191d59eb81
Gulyamov M.A <gulyamov@buroburo.ru>
2025-01-15 22:07:41 +0500

    test5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-// import NotFound from './apps/client/components/NotFound'
+import NotFound from './apps/client/components/NotFound'
 import Login from './apps/admin/pages/Login'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/HomePage'
 import AdminRoutes from './routes/AdminRoutes'
 import VendorRoutes from './routes/VendorRoutes'
 
@@ -13,16 +11,12 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Login />} />
-        {/* <Route path="navbar" element={<Navbar />} /> */}
-        <Route path="home" element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
 
-         {/*client and admin routes  */}
+        {/*client and admin routes  */}
         <Route path="/admin/:res/*" element={<AdminRoutes />} />
         <Route path="vendor/:res/:table?/*" element={<VendorRoutes />} />
-
       </Routes>
 
       <ToastContainer />
