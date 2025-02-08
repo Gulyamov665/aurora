a66b87f4ad95f218c61b99c0c3b47d516325f29e
Gulyamov M.A <gulyamov@buroburo.ru>
2025-02-08 22:15:23 +0500

    fix category Type

--- src/apps/client/modules/order/types/orderTypes.ts
+++ src/apps/client/modules/order/types/orderTypes.ts
@@ -1,13 +1,14 @@
 export type ProductType = {
-  id: number
-  name: string
-  photo: string
-  price: number
-  count: number
-}
+  id: number;
+  name: string;
+  photo: string;
+  price: number;
+  count: number;
+  description: string;
+};
 
 export type OrderProductsProps = {
-  product: ProductType
-  increase: () => void
-  decrease: () => void
-}
+  product: ProductType;
+  increase: () => void;
+  decrease: () => void;
+};
