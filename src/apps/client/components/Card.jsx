853c4f83a5b7bbcd995a2cb8d6bbd16564173e5f
Gulyamov M.A <gulyamov@buroburo.ru>
2025-02-15 22:30:27 +0500

    env added

--- src/apps/client/components/Card.jsx
+++ src/apps/client/components/Card.jsx
@@ -1,52 +1,38 @@
-import React from 'react'
-import CurrencyFormat from 'react-currency-format'
-import { addCartItem } from '../../../store/cartSlice'
-import { useDispatch } from 'react-redux'
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+import { addCartItem } from "../../../store/cartSlice";
+import { useDispatch } from "react-redux";
 
 export default function Card(props) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const addToCart = (event) => {
-    event.stopPropagation()
+    event.stopPropagation();
     const cartItem = {
       ...props,
       count: 1,
-    }
-    dispatch(addCartItem(cartItem))
-  }
+    };
+    dispatch(addCartItem(cartItem));
+  };
 
   return (
     <>
       <div className="card">
-        <img
-          className="card__image"
-          loading="lazy"
-          src={props.photo}
-          alt={props.name}
-        />
+        <img className="card__image" loading="lazy" src={props.photo} alt={props.name} />
         <div className="card__info">
           <div className="car__info--title">
-          <p className="card__info--price">
-              <CurrencyFormat
-                value={props.price}
-                displayType={'text'}
-                thousandSeparator={' '}
-                suffix={' сум'}
-              />
+            <p className="card__info--price">
+              <CurrencyFormat value={props.price} displayType={"text"} thousandSeparator={" "} suffix={" сум"} />
             </p>
             <h3>{props.name}</h3>
           </div>
         </div>
-        <div style={{ textAlign: 'center' }}>
-          <button
-            className="btn card-btn-order"
-            // style={{ width: '90%', marginBottom: "10px" }}
-            onClick={addToCart}
-          >
+        <div style={{ textAlign: "center" }}>
+          <button className="btn card-btn-order" onClick={addToCart}>
             <b>+ Добавить</b>
           </button>
         </div>
       </div>
     </>
-  )
+  );
 }
