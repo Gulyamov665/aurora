f9c11dde56b28efa191022b478d746a09e95b8ae
Gulyamov M.A <gulyamov@buroburo.ru>
2025-02-22 18:25:03 +0500

    fix 425 media screen for card

--- src/apps/client/features/products/Products.tsx
+++ src/apps/client/features/products/Products.tsx
@@ -27,7 +27,7 @@ const Products: FC<ProductsProps> = ({ menuItems, category, sectionRefs, handleV
                   <div
                     onClick={() => handleView(filteredObj)}
                     key={filteredObj.id}
-                    className="col-6 col-sm-6 col-md-4 col-lg-3"
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 "
                   >
                     <Card {...filteredObj} />
                   </div>
