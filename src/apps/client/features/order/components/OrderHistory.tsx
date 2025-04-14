f99292b3d2f85252484cc0ee02565f61e85f5255
Gulyamov M.A <gulyamov@buroburo.ru>
2025-04-15 00:31:46 +0500

    refactor(order-history): improve layout and add fallback image for products
    Restructure the OrderHistory component to enhance readability and add a fallback image for products without a photo. The changes include better organization of the restaurant details and consistent styling for avatars.

--- src/apps/client/features/order/components/OrderHistory.tsx
+++ src/apps/client/features/order/components/OrderHistory.tsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { OrderCard } from "../pages/MyOrders";
 import { OrderHistoryProps } from "../types/orderTypes";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
+import noPhoto from "@/assets/nophoto.jpg";
 
 export const OrderHistory: React.FC<OrderHistoryProps> = ({ order }) => {
   return (
-    <div>
-      <OrderCard sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <h2>{order.restaurant?.name}</h2>
-        <AvatarGroup max={4}>
-          {order.products.map((product) => (
-            <Avatar alt={product.name} src={product.photo} key={product.id} />
-          ))}
-        </AvatarGroup>
-
-        <Typography variant="body1" fontWeight={500}>
-          {Number(order.total_price).toLocaleString()} сум{" "}
+    <OrderCard sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+      <Box>
+        <Typography>Заказ № {order.id}</Typography>
+        <Typography variant="h5" fontWeight={500}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Avatar src={order.restaurant?.photo} sx={{ mr: 1, width: 40, height: 40 }} sizes="s" />
+            <Typography>{order.restaurant?.name}</Typography>
+          </Box>
         </Typography>
-      </OrderCard>
-    </div>
+      </Box>
+      <AvatarGroup max={4}>
+        {order.products.map((product) => (
+          <Avatar sx={{ width: 30, height: 30 }} alt={product.name} src={product.photo || noPhoto} key={product.id} />
+        ))}
+      </AvatarGroup>
+
+      <Typography variant="body1" fontWeight={800}>
+        {Number(order.total_price).toLocaleString()} сум{" "}
+      </Typography>
+    </OrderCard>
   );
 };
