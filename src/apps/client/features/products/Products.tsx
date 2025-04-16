a02b9fe921d10dacaa090fb06bff145bdee7dbdb
Gulyamov M.A <gulyamov@buroburo.ru>
2025-04-16 17:13:49 +0500

    refactor(Products): simplify product props passed to Card component
    Restructure the product object passed to the Card component to include only necessary fields (id, photo, name, price) to improve readability and maintainability

--- src/apps/client/features/products/Products.tsx
+++ src/apps/client/features/products/Products.tsx
@@ -77,7 +77,17 @@ const Products: FC<ProductsProps> = ({ menuItems, category, sectionRefs, handleV
                     key={filteredObj.id}
                     className="col-6 col-sm-6 col-md-4 col-lg-3 "
                   >
-                    <Card product={filteredObj} findItem={findItem} decrease={decrease} addToCart={onClick} />
+                    <Card
+                      product={{
+                        id: filteredObj.id,
+                        photo: filteredObj.photo,
+                        name: filteredObj.name,
+                        price: filteredObj.price,
+                      }}
+                      findItem={findItem}
+                      decrease={decrease}
+                      addToCart={onClick}
+                    />
                   </div>
                 ))}
             </div>
