f9bdc8f44f4dd53259c1ce0a3937e5c2e0507dc3
Gulyamov M.A <gulyamov@buroburo.ru>
2025-04-18 00:36:33 +0500

    feat(map): migrate from Google Maps to OpenStreetMap with Leaflet
    Refactor the map feature to use OpenStreetMap with Leaflet instead of Google Maps. This includes adding new dependencies (`leaflet`, `react-leaflet`, `@types/leaflet`), creating a new `MoveHandler` component, and updating the map UI to support OSM tiles and geolocation. The change simplifies the map integration and removes dependency on Google Maps API.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import store, { persistor } from './store'
-import App from './App'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'react-image-crop/dist/ReactCrop.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store";
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import "react-image-crop/dist/ReactCrop.css";
+import "leaflet/dist/leaflet.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <BrowserRouter
     future={{
@@ -19,9 +20,9 @@ root.render(
     }}
   >
     <Provider store={store}>
-      <PersistGate loading={'Loading...'} persistor={persistor}>
+      <PersistGate loading={"Loading..."} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
   </BrowserRouter>
-)
+);
