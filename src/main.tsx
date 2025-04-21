97fdba6bf8823a7eaf17ca0ddd7c8f8238b422fd
Gulyamov M.A <gulyamov@buroburo.ru>
2025-04-22 02:36:47 +0500

    refactor(main): remove unused PWA registration code

--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "react-image-crop/dist/ReactCrop.css";
 import "leaflet/dist/leaflet.css";
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({ immediate: true });
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
