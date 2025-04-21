3e5081ddea31407f551b9a0433435d118911e338
Gulyamov M.A <gulyamov@buroburo.ru>
2025-04-22 02:33:33 +0500

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "prompt",
-      includeAssets: ["logo.png"],
-      devOptions: {
-        enabled: true, // 💥 это обязательно для локальной разработки!
-      },
-
-      manifest: {
-        name: "Aurora",
-        short_name: "Aurora",
-        start_url: "/vendor/Olivia",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@store": path.resolve(__dirname, "./src/store"), // Алиас для store
