34ac6107b2bdfc561bbeee28c04c6775f4dcfcd9
Gulyamov M.A <gulyamov@buroburo.ru>
2025-06-10 23:55:11 +0500

    fix: Update overflow property in card_view_scrollable for better scrolling behavior

--- src/apps/client/features/card/CardView.tsx
+++ src/apps/client/features/card/CardView.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useState } from "react";
-// import { motion, AnimatePresence, useDragControls } from "framer-motion";
+import { motion, AnimatePresence, useDragControls } from "framer-motion";
 import { CardViewProps } from "./types";
-// import { Dialog } from "@mui/material";
+import { SwipeableDrawer } from "@mui/material";
 import { CardViewContent } from "./components/CardViewContent";
 import { useBreakpoint } from "@/hooks/useBreakpoint";
 import { CardViewContentFullSize } from "./components/CardViewContentFullSize";
@@ -13,7 +13,6 @@ import { useAddToCartMutation } from "@store/admin/api/orders";
 import { authState } from "@store/user/slices/authSlice";
 import { IVariants } from "../order/types/orderTypes";
 import { AppDispatch } from "@store/index";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount }) => {
   const { data } = useOutletContext<OutletContextType>();
@@ -21,7 +20,7 @@ const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount })
   const [addToCart] = useAddToCartMutation();
   const [selectedVariant, setSelectedVariant] = useState<number | null>(null);
   const [option, setOption] = useState<IVariants | null>(null);
-  // const controls = useDragControls();
+  const controls = useDragControls();
   const breakpoint = useBreakpoint();
   const dispatch = useDispatch<AppDispatch>();
 
@@ -60,105 +59,60 @@ const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount })
   };
 
   return (
-    // <AnimatePresence>
-    //   {open && (
-    //     <Dialog open={open} onClose={() => setIsOpen(!open)}>
-    //       <div className="card_modal" onClick={() => setIsOpen(!open)}></div>
+    <AnimatePresence>
+      {open && (
+        <SwipeableDrawer open={open} onClose={() => setIsOpen(!open)} onOpen={() => setIsOpen(!open)}>
+          <div className="card_modal" onClick={() => setIsOpen(!open)}></div>
 
-    //       <motion.div
-    //         drag="y"
-    //         dragControls={controls}
-    //         dragElastic={{ top: 0, bottom: 1 }}
-    //         dragConstraints={{ top: 0, bottom: 0 }}
-    //         onDragEnd={(_, info) => {
-    //           if (info.offset.y > 100) {
-    //             setIsOpen(false);
-    //           }
-    //         }}
-    //         initial={{ y: "100%" }}
-    //         animate={{ y: 0 }}
-    //         exit={{ y: "100%" }}
-    //         transition={{ duration: 0.3 }}
-    //         style={{
-    //           position: "fixed",
-    //           bottom: 0,
-    //           background: "white",
-    //         }}
-    //         className="card_view_motion"
-    //       >
-    //         {item &&
-    //           (breakpoint === "lg" || breakpoint === "xl" ? (
-    //             <CardViewContentFullSize
-    //               item={item}
-    //               count={count}
-    //               setCount={setCount}
-    //               setIsOpen={setIsOpen}
-    //               onAdd={onAddProduct}
-    //               selectedVariant={selectedVariant}
-    //               setSelectedVariant={setSelectedVariant}
-    //               option={option}
-    //             />
-    //           ) : (
-    //             <CardViewContent
-    //               item={item}
-    //               count={count}
-    //               setCount={setCount}
-    //               setIsOpen={setIsOpen}
-    //               onAdd={onAddProduct}
-    //               selectedVariant={selectedVariant}
-    //               setSelectedVariant={setSelectedVariant}
-    //               option={option}
-    //             />
-    //           ))}
-    //       </motion.div>
-    //     </Dialog>
-    //   )}
-    // </AnimatePresence>
-
-    <SwipeableDrawer
-      anchor="bottom"
-      open={open}
-      onClose={() => setIsOpen(false)}
-      onOpen={() => setIsOpen(true)}
-      disableDiscovery={false} // разрешает "потянуть вверх"
-      disableSwipeToOpen={false} // можно свайпом открыть
-      transitionDuration={225}
-      hysteresis={0.55}
-      PaperProps={{
-        sx: {
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-          maxHeight: "90vh",
-        },
-      }}
-    >
-      <div style={{ overflowY: "auto", maxHeight: "90vh" }}>
-        {item &&
-          (breakpoint === "lg" || breakpoint === "xl" ? (
-            <CardViewContentFullSize
-              item={item}
-              count={count}
-              setCount={setCount}
-              setIsOpen={setIsOpen}
-              onAdd={onAddProduct}
-              selectedVariant={selectedVariant}
-              setSelectedVariant={setSelectedVariant}
-              option={option}
-            />
-          ) : (
-            <CardViewContent
-              item={item}
-              count={count}
-              setCount={setCount}
-              setIsOpen={setIsOpen}
-              onAdd={onAddProduct}
-              selectedVariant={selectedVariant}
-              setSelectedVariant={setSelectedVariant}
-              option={option}
-            />
-          ))}
-      </div>
-    </SwipeableDrawer>
+          <motion.div
+            drag="y"
+            dragControls={controls}
+            dragElastic={{ top: 0, bottom: 1 }}
+            dragConstraints={{ top: 0, bottom: 0 }}
+            onDragEnd={(_, info) => {
+              if (info.offset.y > 100) {
+                setIsOpen(false);
+              }
+            }}
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "100%" }}
+            transition={{ duration: 0.3 }}
+            style={{
+              position: "fixed",
+              bottom: 0,
+              background: "white",
+            }}
+            className="card_view_motion"
+          >
+            {item &&
+              (breakpoint === "lg" || breakpoint === "xl" ? (
+                <CardViewContentFullSize
+                  item={item}
+                  count={count}
+                  setCount={setCount}
+                  setIsOpen={setIsOpen}
+                  onAdd={onAddProduct}
+                  selectedVariant={selectedVariant}
+                  setSelectedVariant={setSelectedVariant}
+                  option={option}
+                />
+              ) : (
+                <CardViewContent
+                  item={item}
+                  count={count}
+                  setCount={setCount}
+                  setIsOpen={setIsOpen}
+                  onAdd={onAddProduct}
+                  selectedVariant={selectedVariant}
+                  setSelectedVariant={setSelectedVariant}
+                  option={option}
+                />
+              ))}
+          </motion.div>
+        </SwipeableDrawer>
+      )}
+    </AnimatePresence>
   );
 };
 export default CardView;
