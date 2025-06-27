7d84e2bc1115c25cf32768db6992c4e0daa578fc
Bomuratov <antikko99@gmail.com>
2025-06-28 00:26:09 +0500

    push for publish branch and trello

--- src/apps/admin/features/orders/pages/Orders.tsx
+++ src/apps/admin/features/orders/pages/Orders.tsx
@@ -117,6 +117,7 @@ const Orders: FC = () => {
         soundAllowed={soundAllowed}
       />
 
+
       <div ref={ref} style={{ height: 20 }} />
 
       <Drawer anchor="right" open={drawerOpen} variant="persistent" sx={styles.drawerStyles}>
