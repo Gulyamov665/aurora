d193f036d173ebe9794f1594651bd35425a04fca
Gulyamov M.A <gulyamov@buroburo.ru>
2025-06-30 17:13:39 +0500

    feat: Refactor CardView component for improved readability and functionality

--- src/apps/client/features/card/CardView.tsx
+++ src/apps/client/features/card/CardView.tsx
@@ -1,18 +1,137 @@
+// import { FC, useEffect, useState } from "react";
+// import { motion, AnimatePresence, useDragControls } from "framer-motion";
+// import { CardViewProps } from "./types";
+// import { SwipeableDrawer } from "@mui/material";
+// import { CardViewContent } from "./components/CardViewContent";
+// import { useBreakpoint } from "@/hooks/useBreakpoint";
+// import { CardViewContentFullSize } from "./components/CardViewContentFullSize";
+// import { handleAddToCart, updateCartCache } from "@/Utils/tools";
+// import { OutletContextType } from "../../pages";
+// import { useOutletContext } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useAddToCartMutation } from "@store/admin/api/orders";
+// import { authState } from "@store/user/slices/authSlice";
+// import { IVariants } from "../order/types/orderTypes";
+// import { AppDispatch } from "@store/index";
+
+// const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount }) => {
+//   const { data } = useOutletContext<OutletContextType>();
+//   const { isUser } = useSelector(authState);
+//   const [addToCart] = useAddToCartMutation();
+//   const [selectedVariant, setSelectedVariant] = useState<number | null>(null);
+//   const [option, setOption] = useState<IVariants | null>(null);
+//   const controls = useDragControls();
+//   const breakpoint = useBreakpoint();
+//   const dispatch = useDispatch<AppDispatch>();
+
+//   // устанавливаем значение поумолчанию варианта при открытии модального окна
+//   useEffect(() => {
+//     if (item && item.options?.variants.length > 0) {
+//       const filteredVariants = item.options.variants.filter((v) => v.is_active);
+//       setSelectedVariant(filteredVariants[0].id);
+//     }
+//   }, [item]);
+
+//   // устанавливаем опцию при изменении выбранного варианта
+//   useEffect(() => {
+//     if (selectedVariant && item) {
+//       const options = item.options?.variants.find((v) => v.id === selectedVariant);
+//       setOption(options ?? null);
+//     }
+//   }, [selectedVariant, item]);
+
+//   const onAddProduct = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     if (!item) return;
+//     if (!item || !isUser?.user_id || !data?.id) return;
+
+//     const productData = {
+//       id: item.id,
+//       name: item.name,
+//       price: item.price,
+//       photo: item.photo,
+//       options: option,
+//     };
+
+//     updateCartCache(dispatch, isUser.user_id, data.id, productData);
+
+//     handleAddToCart({ event, productData, quantity: count, userId: isUser?.user_id, restaurantId: data.id, addToCart });
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <AnimatePresence>
+//       {open && (
+//         <SwipeableDrawer open={open} onClose={() => setIsOpen(!open)} onOpen={() => setIsOpen(!open)}>
+//           <div className="card_modal" onClick={() => setIsOpen(!open)}></div>
+
+//           <motion.div
+//             drag="y"
+//             dragControls={controls}
+//             dragElastic={{ top: 0, bottom: 1 }}
+//             dragConstraints={{ top: 0, bottom: 0 }}
+//             onDragEnd={(_, info) => {
+//               if (info.offset.y > 100) {
+//                 setIsOpen(false);
+//               }
+//             }}
+//             initial={{ y: "100%" }}
+//             animate={{ y: 0 }}
+//             exit={{ y: "100%" }}
+//             transition={{ duration: 0.3 }}
+//             style={{
+//               position: "fixed",
+//               bottom: 0,
+//               background: "white",
+//             }}
+//             className="card_view_motion"
+//           >
+//             {item &&
+//               (breakpoint === "lg" || breakpoint === "xl" ? (
+//                 <CardViewContentFullSize
+//                   item={item}
+//                   count={count}
+//                   setCount={setCount}
+//                   setIsOpen={setIsOpen}
+//                   onAdd={onAddProduct}
+//                   selectedVariant={selectedVariant}
+//                   setSelectedVariant={setSelectedVariant}
+//                   option={option}
+//                 />
+//               ) : (
+//                 <CardViewContent
+//                   item={item}
+//                   count={count}
+//                   setCount={setCount}
+//                   setIsOpen={setIsOpen}
+//                   onAdd={onAddProduct}
+//                   selectedVariant={selectedVariant}
+//                   setSelectedVariant={setSelectedVariant}
+//                   option={option}
+//                 />
+//               ))}
+//           </motion.div>
+//         </SwipeableDrawer>
+//       )}
+//     </AnimatePresence>
+//   );
+// };
+// export default CardView;
+
 import { FC, useEffect, useState } from "react";
-import { motion, AnimatePresence, useDragControls } from "framer-motion";
-import { CardViewProps } from "./types";
+import { motion, AnimatePresence } from "framer-motion";
 import { SwipeableDrawer } from "@mui/material";
-import { CardViewContent } from "./components/CardViewContent";
 import { useBreakpoint } from "@/hooks/useBreakpoint";
+import { CardViewContent } from "./components/CardViewContent";
 import { CardViewContentFullSize } from "./components/CardViewContentFullSize";
 import { handleAddToCart, updateCartCache } from "@/Utils/tools";
-import { OutletContextType } from "../../pages";
 import { useOutletContext } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useAddToCartMutation } from "@store/admin/api/orders";
 import { authState } from "@store/user/slices/authSlice";
-import { IVariants } from "../order/types/orderTypes";
+import { OutletContextType } from "../../pages";
 import { AppDispatch } from "@store/index";
+import { CardViewProps } from "./types";
+import { IVariants } from "../order/types/orderTypes";
 
 const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount }) => {
   const { data } = useOutletContext<OutletContextType>();
@@ -20,28 +139,24 @@ const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount })
   const [addToCart] = useAddToCartMutation();
   const [selectedVariant, setSelectedVariant] = useState<number | null>(null);
   const [option, setOption] = useState<IVariants | null>(null);
-  const controls = useDragControls();
   const breakpoint = useBreakpoint();
   const dispatch = useDispatch<AppDispatch>();
 
-  // устанавливаем значение поумолчанию варианта при открытии модального окна
   useEffect(() => {
-    if (item && item.options?.variants.length > 0) {
-      const filteredVariants = item.options.variants.filter((v) => v.is_active);
-      setSelectedVariant(filteredVariants[0].id);
+    if (item?.options?.variants.length) {
+      const firstActive = item.options.variants.find((v) => v.is_active);
+      setSelectedVariant(firstActive?.id ?? null);
     }
   }, [item]);
 
-  // устанавливаем опцию при изменении выбранного варианта
   useEffect(() => {
     if (selectedVariant && item) {
-      const options = item.options?.variants.find((v) => v.id === selectedVariant);
-      setOption(options ?? null);
+      const foundOption = item.options?.variants.find((v) => v.id === selectedVariant);
+      setOption(foundOption ?? null);
     }
   }, [selectedVariant, item]);
 
   const onAddProduct = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (!item) return;
     if (!item || !isUser?.user_id || !data?.id) return;
 
     const productData = {
@@ -53,36 +168,34 @@ const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount })
     };
 
     updateCartCache(dispatch, isUser.user_id, data.id, productData);
-
-    handleAddToCart({ event, productData, quantity: count, userId: isUser?.user_id, restaurantId: data.id, addToCart });
+    handleAddToCart({ event, productData, quantity: count, userId: isUser.user_id, restaurantId: data.id, addToCart });
     setIsOpen(false);
   };
 
   return (
     <AnimatePresence>
       {open && (
-        <SwipeableDrawer open={open} onClose={() => setIsOpen(!open)} onOpen={() => setIsOpen(!open)}>
-          <div className="card_modal" onClick={() => setIsOpen(!open)}></div>
-
+        <SwipeableDrawer
+          open={open}
+          onClose={() => setIsOpen(false)}
+          onOpen={() => setIsOpen(true)}
+          disableBackdropTransition
+          disableDiscovery
+          anchor="bottom"
+          PaperProps={{
+            style: {
+              borderTopLeftRadius: 16,
+              borderTopRightRadius: 16,
+              maxHeight: "90vh",
+              overflow: "auto",
+            },
+          }}
+        >
           <motion.div
-            drag="y"
-            dragControls={controls}
-            dragElastic={{ top: 0, bottom: 1 }}
-            dragConstraints={{ top: 0, bottom: 0 }}
-            onDragEnd={(_, info) => {
-              if (info.offset.y > 100) {
-                setIsOpen(false);
-              }
-            }}
-            initial={{ y: "100%" }}
+            initial={{ y: 100 }}
             animate={{ y: 0 }}
-            exit={{ y: "100%" }}
+            exit={{ y: 100 }}
             transition={{ duration: 0.3 }}
-            style={{
-              position: "fixed",
-              bottom: 0,
-              background: "white",
-            }}
             className="card_view_motion"
           >
             {item &&
@@ -115,4 +228,5 @@ const CardView: FC<CardViewProps> = ({ item, open, setIsOpen, count, setCount })
     </AnimatePresence>
   );
 };
+
 export default CardView;
